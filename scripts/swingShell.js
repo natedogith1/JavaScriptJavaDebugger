7a370d17a0b314efc1880732de15748f1ad8b332
nafaulkn <nafaulkn@ncsu.edu>
2016-08-11 20:03:00 -0400

    evaluation seems to be working now

--- scripts/swingShell.js
+++ scripts/swingShell.js
@@ -7,58 +7,92 @@ Java.type("javax.swing.SwingUtilities").invokeLater(function(){
   var ScrollPaneConstants = Java.type("javax.swing.ScrollPaneConstants");
   var KeyListener = Java.type("java.awt.event.KeyListener");
   var KeyEvent = Java.type("java.awt.event.KeyEvent");
+  var SwingWorker = Java.type("javax.swing.SwingWorker");
   
   // actual code
   var frame = new JFrame("debugger shell");
   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   var document_super;
+  var prompt = "> ";
   var documentObject = {
-    prompt: "> ",
     outputEnd: null,
-    promptStart: null,
-    inputStart: null,
+    isFirst: true,
     insertString: function(offs, str, a) {
-      if ( offs >= this.inputStart.getOffset() )
+      if ( offs >= this.outputEnd.getOffset() + prompt.length )
         document_super.insertString(offs, str, a);
     },
     remove: function(offs, len) {
-      if ( offs >= this.inputStart.getOffset() )
+      if ( offs >= this.outputEnd.getOffset() + prompt.length )
         document_super.remove(offs, len);
     },
     outputText: function(text) {
       document_super.insertString(this.outputEnd.getOffset(), text, null)
+      if ( this.isFirst & text.length > 0) {
+        this.isFirst = false;
+        document_super.insertString(this.outputEnd.getOffset(), "\n", null)
+        prompt = "\n" + prompt;
+        this.outputEnd = document_super.createPosition(text.length);
+      }
     },
     getInput: function() {
-      var text = document_super.getText(inputStart.getOffset(), document_super.getLength() - inputStart.getOffset());
-      outputText(document_super.getText(promptStart.getOffset(), document_super.getLength() - promptStart.getOffset()));
-      document_super.remove(promptStart.getOffset(), document_super.getLength() - promptStart.getOffset());
-      document_super.insertText(promptStart.getOffset(), this.prompt, null);
-      inputStart = document_super.createPosition(promptStart.getOffset() + prompt.length);
-      return text;
+      if ( this.isFirst ) {
+        this.isFirst = false;
+        var text = document_super.getText(prompt.length, document_super.getLength() - prompt.length);
+        document_super.insertString(document_super.getLength(), "\n" + prompt, null);
+        prompt = "\n" + prompt
+        this.outputEnd = document_super.createPosition(document_super.getLength() - prompt.length);
+        return text;
+      } else {
+        var text = document_super.getText(this.outputEnd.getOffset() + prompt.length, document_super.getLength() - this.outputEnd.getOffset() - prompt.length);
+        this.outputText("\n" + document_super.getText(this.outputEnd.getOffset() + 1, document_super.getLength() - this.outputEnd.getOffset() - 1));
+        document_super.remove(this.outputEnd.getOffset() + prompt.length, document_super.getLength() - this.outputEnd.getOffset() - prompt.length);
+        return text;
+      }
     }
   };
   var document = new (Java.extend(PlainDocument))(documentObject);
   document_super = Java.super(document);
-  document_super.insertString(0, "\n" + documentObject.prompt, null);
+  document_super.insertString(0, prompt, null);
   documentObject.outputEnd = document_super.createPosition(0);
-  documentObject.promptStart = document_super.createPosition(1);
-  documentObject.inputStart = document_super.createPosition(1 - 1 + documentObject.prompt.length);
   var textArea = new JTextArea(document, null, 20, 50);
   textArea.setLineWrap(true);
+  var swingWorkerAdapter = Java.extend(SwingWorker);
+  var shellGlobal = {};
   textArea.addKeyListener(new KeyListener(){
     keyPressed : function(event) {
-      if ( event.
+      if ( event.getKeyCode() == KeyEvent.VK_ENTER ) {
+        var text = documentObject.getInput();
+        print(text);
+        (new swingWorkerAdapter() {
+          doInBackground: function() {
+            try {
+              eval.call(shellGlobal,text)
+            } catch( e ) {
+              print( e );
+            }
+          }
+        }).execute();
+        event.consume();
+      } else if ( event.getKeyCode() == KeyEvent.VK_LEFT ) {
+        if ( textArea.getCaretPosition() == documentObject.outputEnd.getOffset() + prompt.length ) {
+          event.consume();
+        }
+      }
+        
     },
     keyReleased : function(event) {
       
     },
     keyTyped : function(event) {
-      if ( textArea.getCaretPosition() < documentObject.inputStart.getOffset() )
-        textArea.setCaret(document.getLength());
+      if ( textArea.getCaretPosition() < documentObject.outputEnd.getOffset() + prompt.length )
+        textArea.setCaretPosition(document.getLength());
     }
   });
+  textArea.setCaretPosition(document.getLength());
   var scrollPane = new JScrollPane(textArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
   frame.add(scrollPane, null);
   frame.pack();
   frame.setVisible(true);
+  
+  
 });
